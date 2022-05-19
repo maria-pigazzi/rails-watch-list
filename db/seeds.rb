d3273eca0f17eeef15bde01194922cbfac6452a7
maria pigazzi <maria.pigazzi98@gmail.com>
2022-05-19 13:51:55 +0200

    please save

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated?api_key=<your_api_key"
+what_is_this_even = URI.open(url).read
+film = JSON.parse(what_is_this_even)
+
+puts "#{film["name"]} - #{film["bio"]}"
