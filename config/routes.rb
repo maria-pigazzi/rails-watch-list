c9b3dd096b3fdb7fa905be5aae7e5bd266c9a73f
maria pigazzi <maria.pigazzi98@gmail.com>
2022-05-20 12:40:08 +0200

    show lists done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [ :new, :create, :show, :index ]
+  root to: 'lists#index'
+  resources :lists, only: [ :new, :create, :show ] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
